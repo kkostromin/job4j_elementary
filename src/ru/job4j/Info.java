13bfc70761942065146225f0777a28eaf45a147e
KonstantinK <k.kostromin@bk.ru>
2020-11-24 13:29:08 +0300

    3.4. Заголовок в файле java [#416625]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+/**
+ * Created by kkostromin on 11/24/2020.
+ */
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("24.11.2020");
+    }
+}
